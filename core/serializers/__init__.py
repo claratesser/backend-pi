3a1be2007418b49d0eaac663b979e9014ab06faa
claratesser <claratesserc@gmail.com>
2026-04-15 09:23:19 -0300

    feat: alterações

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -4,4 +4,4 @@ from .categoria import CategoriaSerializer
 from .produto import ProdutoSerializer
 from .pagamento import PagamentoSerializer
 from .pedido import PedidoSerializer
-from .item_pedido import ItemPedido
+from .item_pedido import ItemPedidoSerializer
